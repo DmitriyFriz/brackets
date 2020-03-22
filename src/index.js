4d2077ace205687e0c9c66bf8aeb5af90e1f696a
DmitriyFriz <dmitriy.bulavko.friz44@gmail.com>
2020-03-22 21:14:46 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+    bracketsConfig = bracketsConfig.reduce((newBracketsConfig, item) => { 
+    newBracketsConfig.push(item.join('')) ;
+    return newBracketsConfig;
+    }, []);
+
+    let checkedStr;
+    while (checkedStr != str) {
+        checkedStr = str;
+        bracketsConfig.forEach(bracket => {
+            str = str.replace(bracket,'');
+        });
+    }
+
+    return !str;
 }
+
